2046489c3883fa89c4e7586a05e1c4e4bbd23074
Ivy Chen <3833878+mhichen@users.noreply.github.com>
2020-12-12 14:10:33 -0600

    Better pass at word concatenation.

--- Sliding_Window/word_concatenation.py
+++ Sliding_Window/word_concatenation.py
@@ -6,9 +6,10 @@
 # without any overlapping of words.  It is given that all words
 # are of the same length
 
-# Time complexity: O(N / K), where N is the number of
-# characters in the string and K is the length of the words
-# Space complexity: O(M + N), where M is the total number of words
+# Time complexity: O(N * M * K), where N is the number of
+# characters in the string, M is the total number of words
+# and K is the length of the words
+# Space complexity: O(M + N)
 def word_concatenation(st, words):
 
     # Store words into hash table
@@ -22,34 +23,45 @@ def word_concatenation(st, words):
     # Variable to store length of words, which
     # are all the same length
     k = len(words[0])
+
+    # Store number of words
+    nwords = len(words)
     
     # Keep track of number of word matches
     match = 0
 
-    # Index to track beginning of window
-    i = 0
-
     # Array to track resuls
     results = []
 
-    for j in range(0, len(st), k):
+    # only need to iterate i to beginning of last
+    # possible string of concatenated words
+    for i in range((len(st) - nwords * k) + 1):
 
-        w = st[j:j+k]
+        # Keep track of words encountered
+        words_seen = {}
+        
+        for j in range(nwords):
 
-        if w in freq:
-            freq[w] -= 1
+            next_word_index = i + j * k
             
-            if freq[w] == 0:
-                match += 1
-        print(freq)
+            w = st[next_word_index:next_word_index + k]
 
-        if match == len(freq):
-            results += [i]
+            # if the word is not in our dictionary of required
+            # words, don't need this word, so skip
+            if w not in freq:
+                break
 
-            freq[st[i:i+k]] += 1
-            match -=1
+            if w not in words_seen:
+                words_seen[w] = 0
+            words_seen[w] += 1
 
-            i += k
+            # If word frequency is higher than that in dictionary
+            # of required words, break
+            if words_seen[w] > freq.get(w, 0):
+                break
+
+            if j + 1 == nwords:
+                results += [i]
 
     return results
     
